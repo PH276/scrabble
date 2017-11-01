61f843d48330f762b83d227703b0f2a030d7264e
PH276 <pascal.huitorel@lepoles.com>
2017-11-01 15:48:22 +0100

    commit 1
    plateu de jeu complet

--- index.php
+++ index.php
@@ -23,11 +23,26 @@
                 <?php for ($i = 0 ; $i < 15 ; $i++) : ?>
                     <tr>
                         <?php for ($j = 0 ; $j < 15 ; $j++) : ?>
-                            <?php if (($i==$j || $i==14-$j) && $i>0 && $i<14 )  : ?>
+                            <?php if ($i%7 == 0 && $j%7 == 0) : ?>
+                                <?php if ($i!=7 || $j!=7) : ?>
+                                    <td class="mot-triple"></td>
+                                <?php else : ?>
+                                    <td class="mot-double"></td>
+                                <?php endif; ?>
+                            <?php elseif (($i==$j && (($i>0 && $i<5) || ($i>9 && $i<14))) ||
+                                        ($i==14-$j && (($i>0 && $i<5) || ($i>9 && $i<14)))) :  ?>
                                 <td class="mot-double"></td>
-                            <?php elseif (in_array($i, $motTriple) && in_array($j, $motTriple))  : ?>
-                                <td class="mot-triple"></td>
-                            <?php elseif (in_array($i, $lettreTriple) && in_array($j, $lettreTriple))  : ?>
+                            <?php elseif (($i%7 == 0 && $j == 3)
+                            || ($i == 3 && $j%7 == 0)
+                            || ($i%7 == 0 && $j == 11)
+                            || ($i == 11 && $j%7 == 0)
+                            || (($i-2)%4 == 0 && ($j-2)%4 == 0 && $i<7 && $j<7)
+                            || ((12-$i)%4 == 0 && ($j-2)%4 == 0 && $i>7 && $j<7)
+                            || (($i-2)%4 == 0 && (12-$j)%4 == 0 && $i<7 && $j>7)
+                            || ((12-$i)%4 == 0 && (12-$j)%4 == 0 && $i>7 && $j>7)
+                            ) : ?>
+                                <td class="lettre-double"></td>
+                            <?php elseif (($i-1)%4 == 0 && ($j-1)%4 == 0) : ?>
                                 <td class="lettre-triple"></td>
                             <?php else : ?>
                                 <td></td>
